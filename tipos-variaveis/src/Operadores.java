3f7401ec301357c4714f2eee97bdca6e968df0be
Marcelo <marcelocjava@gmail.com>
2023-03-05 21:39:29 -0300

    tipos variaveis

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,25 @@
+import java.sql.Date;
+
+public class Operadores {
+ public static void main(String[] args) {
+    String nomeCompleto = " LINGUAGEM " + " JAVA ";
+    System.out.println(nomeCompleto);
+
+String nome = "Marcelo";
+int idade = 45;
+double peso = 85.4;
+char sexo = 'M';
+boolean doadorOrgao = false;
+Date dataNascimento = new Date(25);
+
+double soma = 10.5 + 15.7;
+int subtração = 113 - 25;
+int multiplicação = 20 * 7;
+int divisao = 15/ 3;
+int modulo = 18 % 3;
+double resultado = ( 18*7 )+ ( 28/4 );
+
+String concatenação = 1 + "1" + 1 + "1";
+System.out.println(concatenação);
+ }  
+}
